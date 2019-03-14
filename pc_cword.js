4ae71ce8ae5167e85c245131b51b0c5ea3677993
Brenden Riley <briley001@west-mec.org>
2019-03-14 15:28:10 -0700

    Name changes and vars
    Changed file names and added author names and date. Declared multiple variables and set up the init() function to run when the window loads.

--- pc_cword.js
+++ pc_cword.js
@@ -7,8 +7,8 @@
 
    Crossword Puzzle Script
    
-   Author: 
-   Date:   
+   Author: Brenden Riley
+   Date:   3.14.19
    
    Global Variables
    ================
@@ -49,11 +49,30 @@
 
 */
 
+var allLetters;
+var currentLetter;
+var wordLetters;
+var acrossClue;
+var downClue;
+var typeDirection = "right";
 
+window.onload = init();
 
+function init() {
+      allLetters = document.querySelectorAll("table#crossword span")
+      currentLetter = allLetters[0];
+      var acrossID = currentLetter.getAttribute("data-clue-a");
+      var downID = currentLetter.getAttribute("data-clue-d");
+      acrossClue = document.getElementById("acrossID");
+      downClue = document.getElementById("downID");
+}
 
-
-   
+function formatPuzzle(puzzleLetter) {
+      currentLetter = puzzleLetter;
+      for (var i = 0; i < allLetters.length; i++) {
+            allLetters[i].backgroundColor = "";
+      }
+}
 
 
 
@@ -62,5 +81,5 @@
 /*====================================================*/
 
 function getChar(keyNum) {
-   return String.fromCharCode(keyNum);
+      return String.fromCharCode(keyNum);
 }
